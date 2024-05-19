af686e3aabf23fbfbd8450f1b276168331216f96
Abdiel Sanchez-Gaud <gaud.abdiel@gmail.com>
2024-05-19 13:51:17 -0500

    Update EmployeesController to use EmployeeService

--- PaylocityBenefitsCalculator/Api/Controllers/EmployeesController.cs
+++ PaylocityBenefitsCalculator/Api/Controllers/EmployeesController.cs
@@ -1,6 +1,6 @@
-﻿using Api.Dtos.Dependent;
-using Api.Dtos.Employee;
+﻿using Api.Dtos.Employee;
 using Api.Models;
+using Api.Services;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 
@@ -10,20 +10,47 @@ namespace Api.Controllers;
 [Route("api/v1/[controller]")]
 public class EmployeesController : ControllerBase
 {
+    private readonly IEmployeeService _employeeService;
+    public EmployeesController(IEmployeeService employeeService)
+    {
+        _employeeService = employeeService;
+    }
+
     [SwaggerOperation(Summary = "Get employee by id")]
     [HttpGet("{id}")]
     public async Task<ActionResult<ApiResponse<GetEmployeeDto>>> Get(int id)
     {
-        throw new NotImplementedException();
+        var employee = await _employeeService.GetByIdAsync(id);
+
+        if (employee == null)
+        {
+            return NotFound();
+        }
+
+        return new ApiResponse<GetEmployeeDto>
+        {
+            Data = employee,
+            Success = true
+        };
     }
 
     [SwaggerOperation(Summary = "Get all employees")]
     [HttpGet("")]
     public async Task<ActionResult<ApiResponse<List<GetEmployeeDto>>>> GetAll()
     {
-        // task: use a more realistic production approach
-        // completed: moved data to a data provider
+        // task: use a more realistic production approach -- completed: moved data to a data provider
 
-        throw new NotImplementedException();
+        var employees = await _employeeService.GetAllAsync();
+
+        if (!employees.Any())
+        {
+            return NotFound();
+        }
+
+        return new ApiResponse<List<GetEmployeeDto>>
+        {
+            Data = employees,
+            Success = true
+        };
     }
 }
