e57b2092e76a9a7bc2c7b7e0b2851cc9fad87366
Abdiel Sanchez-Gaud <gaud.abdiel@gmail.com>
2024-05-19 13:50:54 -0500

    Update DependentsController to use DependentService

--- PaylocityBenefitsCalculator/Api/Controllers/DependentsController.cs
+++ PaylocityBenefitsCalculator/Api/Controllers/DependentsController.cs
@@ -1,5 +1,6 @@
 ﻿using Api.Dtos.Dependent;
 using Api.Models;
+using Api.Services;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 
@@ -9,17 +10,38 @@ namespace Api.Controllers;
 [Route("api/v1/[controller]")]
 public class DependentsController : ControllerBase
 {
+    private readonly IDependentService _dependentService;
+    public DependentsController(IDependentService dependentService)
+    {
+        _dependentService = dependentService;
+    }
+
     [SwaggerOperation(Summary = "Get dependent by id")]
     [HttpGet("{id}")]
     public async Task<ActionResult<ApiResponse<GetDependentDto>>> Get(int id)
     {
-        throw new NotImplementedException();
+        var dependent = await _dependentService.GetByIdAsync(id);
+        if (dependent == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(new ApiResponse<GetDependentDto>
+        {
+            Data = dependent,
+            Success = true
+        });
     }
 
     [SwaggerOperation(Summary = "Get all dependents")]
     [HttpGet("")]
     public async Task<ActionResult<ApiResponse<List<GetDependentDto>>>> GetAll()
     {
-        throw new NotImplementedException();
+        var dependents = await _dependentService.GetAllAsync();
+        return Ok(new ApiResponse<List<GetDependentDto>>
+        {
+            Data = dependents.ToList(),
+            Success = true
+        });
     }
 }
