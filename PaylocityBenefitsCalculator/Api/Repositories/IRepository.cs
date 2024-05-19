cb35b58e18f04b74351bf1fb0dee916c08267178
Abdiel Sanchez-Gaud <gaud.abdiel@gmail.com>
2024-05-19 12:28:19 -0500

    Initialize repository layer

---
+++ PaylocityBenefitsCalculator/Api/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Api.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        /*
+         * I'm creating a repository layer to handle communication with the data provider.
+         * This should make it so that I can test or swap the repository separately.
+         * 
+         * Normally I would have a method for each CRUD operation, but for now I'll only use GetAll and GetById.
+         */
+
+        Task<List<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+    }
+}
