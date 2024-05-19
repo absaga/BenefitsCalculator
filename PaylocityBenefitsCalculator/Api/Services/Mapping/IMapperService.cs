64025d9d222aeb7c4d1547727c051906491a1e07
Abdiel Sanchez-Gaud <gaud.abdiel@gmail.com>
2024-05-19 13:37:59 -0500

    Create basic mapper service

---
+++ PaylocityBenefitsCalculator/Api/Services/Mapping/IMapperService.cs
@@ -0,0 +1,17 @@
+﻿using Api.Dtos.Dependent;
+using Api.Dtos.Employee;
+using Api.Models;
+
+namespace Api.Services.Mapping
+{
+    public interface IMapperService
+    {
+        /*
+         * I'm implement a basic mapping service as I'm not too happy with doing this inside the services.
+         * This also seems simple enough to do it without any library, but we'll see if that changes.
+         * 
+         */
+        GetEmployeeDto MapEmployeeToDto(Employee employee);
+        GetDependentDto MapDependentToDto(Dependent dependent);
+    }
+}
---
+++ PaylocityBenefitsCalculator/Api/Services/Mapping/MapperService.cs
@@ -0,0 +1,33 @@
+﻿using Api.Dtos.Dependent;
+using Api.Dtos.Employee;
+using Api.Models;
+
+namespace Api.Services.Mapping
+{
+    public class MapperService : IMapperService
+    {
+        public GetEmployeeDto MapEmployeeToDto(Employee employee)
+        {
+            return new GetEmployeeDto
+            {
+                Id = employee.Id,
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                Salary = employee.Salary,
+                DateOfBirth = employee.DateOfBirth,
+            };
+        }
+
+        public GetDependentDto MapDependentToDto(Dependent dependent)
+        {
+            return new GetDependentDto
+            {
+                Id = dependent.Id,
+                FirstName = dependent.FirstName,
+                LastName = dependent.LastName,
+                DateOfBirth = dependent.DateOfBirth,
+                Relationship = dependent.Relationship,
+            };
+        }
+    }
+}
