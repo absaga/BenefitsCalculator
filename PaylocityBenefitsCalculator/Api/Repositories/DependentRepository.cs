db9908fae8548e14ed898e4b63e142a3a73dc528
Abdiel Sanchez-Gaud <gaud.abdiel@gmail.com>
2024-05-19 12:50:08 -0500

    Create dependent repository

---
+++ PaylocityBenefitsCalculator/Api/Repositories/DependentRepository.cs
@@ -0,0 +1,38 @@
+﻿using Api.Data;
+using Api.Models;
+
+namespace Api.Repositories
+{
+    public class DependentRepository : IRepository<Dependent>
+    {
+        /*
+         * A DB context would probably go here, but I'm using the provided employee list. 
+         * I moved that list into a separate class called ProvidedDataProvider inside the Data folder. 
+         * There's more info on why I did this is in that file and the IDataProvider file.
+         * 
+         */
+
+        private readonly IDataProvider _dataProvider;
+
+        public DependentRepository(IDataProvider dataProvider)
+        {
+            _dataProvider = dataProvider;
+        }
+        public async Task<List<Dependent>> GetAllAsync()
+        {
+            return await Task.FromResult(_dataProvider.GetDependents());
+        }
+        public async Task<Dependent?> GetByIdAsync(int id)
+        {
+            return await Task.FromResult(_dataProvider.GetDependents().FirstOrDefault(d => d.Id == id));
+        }
+        public async Task<List<Dependent>> GetByEmployeeId(int employeeId)
+        {
+            return await Task.FromResult(_dataProvider.GetDependents().Where(d => d.EmployeeId == employeeId).ToList());
+        }
+
+
+
+
+    }
+}
---
+++ PaylocityBenefitsCalculator/Api/Repositories/IDependentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Api.Models;
+
+namespace Api.Repositories
+{
+    /*
+     * I'm creating a separate repository for dependents as I'll need a way to get these by
+     * the employee ID. This method would not be needed in the employee repository.
+     * 
+     */
+
+    public interface IDependentRepository : IRepository<Dependent>
+    {
+        Task<List<Dependent>> GetByEmployeeId(int employeeId);
+    }
+}
