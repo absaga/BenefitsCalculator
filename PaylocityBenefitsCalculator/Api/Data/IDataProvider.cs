d04add8ec951b36345546fcaeb1975fdc0c60d83
Abdiel Sanchez-Gaud <gaud.abdiel@gmail.com>
2024-05-19 12:28:45 -0500

    Initialize data providers layer

---
+++ PaylocityBenefitsCalculator/Api/Data/IDataProvider.cs
@@ -0,0 +1,18 @@
+﻿using Api.Models;
+
+namespace Api.Data
+{
+    public interface IDataProvider
+    {
+        /*
+         * I'm creating this to put the already provided list of employees and dependents.
+         * I'm thinking that if I want to later change the data or create some type of automated testing that
+         * creates different lists of employees and dependents, I could just create a new data provider 
+         * with the mock data and inject it.
+         * 
+         */
+
+        List<Employee> GetEmployees();
+        List<Dependent> GetDependents();
+    }
+}
