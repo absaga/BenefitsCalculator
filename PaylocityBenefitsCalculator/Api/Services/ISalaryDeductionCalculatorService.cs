95c718bdd7f0d1b14287fa6c8551d70a9dfec482
Abdiel Sanchez-Gaud <gaud.abdiel@gmail.com>
2024-05-19 22:19:09 -0500

    Create interface for service that will calculate salary deductions

---
+++ PaylocityBenefitsCalculator/Api/Services/ISalaryDeductionCalculatorService.cs
@@ -0,0 +1,14 @@
+﻿using Api.Models;
+
+namespace Api.Services
+{
+    public interface ISalaryDeductionCalculatorService
+    {
+        /*
+         * I'm thinking of creating this service to calculate the deductions for the employee on a yearly basis.
+         * 
+         */
+
+        List<Deduction> CalculateYearlyDeduction(Employee employee);
+    }
+}
