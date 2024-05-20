a7b456e2d4caf2dd4d67f92c9c1654043d808f2d
Abdiel Sanchez-Gaud <gaud.abdiel@gmail.com>
2024-05-19 22:59:54 -0500

    Implement unit tests for base benefit cost rule

---
+++ PaylocityBenefitsCalculator/ApiTests/Unit Tests/SalaryDeductionRules/BaseBenefitCostRuleTests.cs
@@ -0,0 +1,35 @@
+﻿using Api.Models;
+using Api.Rules.SalaryDeduction;
+using Xunit;
+
+namespace ApiTests.UnitTests
+{
+    public class BaseBenefitCostRuleTests : RuleTest
+    {
+        // This one is the simplest rule since it applies to everyone, so we'll start with it
+
+        [Fact]
+        public void ShouldReturnCorrectAmount()
+        {
+
+            var rule = new BaseBenefitCostRule(MockOptions.Object);
+            var employee = new Employee();
+
+            var deduction = rule.CalculateYearlyDeduction(employee);
+
+            Assert.Equal(SalaryDeductionSettings.BaseBenefitsCost * 12, deduction.Amount);
+            Assert.Equal(PaymentFrequency.Yearly, deduction.Frequency);
+        }
+
+        [Fact]
+        public void ShouldAlwaysReturnTrue()
+        {
+            var rule = new BaseBenefitCostRule(MockOptions.Object);
+            var employee = new Employee();
+
+            var appliestoEmployee = rule.AppliesToEmployee(employee);
+
+            Assert.True(appliestoEmployee);
+        }
+    }
+}
---
+++ PaylocityBenefitsCalculator/ApiTests/Unit Tests/SalaryDeductionRules/RuleTest.cs
@@ -0,0 +1,36 @@
+﻿using Api.Configuration;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+
+namespace ApiTests
+{
+    public class RuleTest
+    {
+        /*
+         * I'm creating this base rule test class with some common Moq properties
+         * and Random because we love Random
+         * 
+         */
+        protected Mock<IOptions<SalaryDeductionSettings>> MockOptions { get; private set; }
+        protected SalaryDeductionSettings SalaryDeductionSettings { get; private set; }
+        protected Random Random { get; private set; }
+
+        protected RuleTest()
+        {
+            SalaryDeductionSettings = new SalaryDeductionSettings
+            {
+                BaseBenefitsCost = 1000m,
+                BaseDependentCost = 600m,
+                OverFiftyDependentFee = 200m,
+                HighSalaryThreshold = 80000m,
+                HighSalaryDeductionPercent = 0.02m
+            };
+
+            MockOptions = new Mock<IOptions<SalaryDeductionSettings>>();
+            MockOptions.Setup(o => o.Value).Returns(SalaryDeductionSettings);
+            Random = new Random();
+        }
+
+    }
+}
