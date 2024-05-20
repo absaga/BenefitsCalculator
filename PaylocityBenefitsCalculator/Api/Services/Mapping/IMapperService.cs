7d01d70cbe4e0ef22b23cc62e5fe15cc118fe4c6
Abdiel Sanchez-Gaud <gaud.abdiel@gmail.com>
2024-05-20 13:07:16 -0500

    Add new mapping classes for paychecks

--- PaylocityBenefitsCalculator/Api/Services/Mapping/IMapperService.cs
+++ PaylocityBenefitsCalculator/Api/Services/Mapping/IMapperService.cs
@@ -1,5 +1,6 @@
 ﻿using Api.Dtos.Dependent;
 using Api.Dtos.Employee;
+using Api.Dtos.Paycheck;
 using Api.Models;
 
 namespace Api.Services.Mapping
@@ -7,11 +8,16 @@ namespace Api.Services.Mapping
     public interface IMapperService
     {
         /*
-         * I'm implement a basic mapping service as I'm not too happy with doing this inside the services.
-         * This also seems simple enough to do it without any library, but we'll see if that changes.
+         * I'm implement a basic mapping service as I'm not too happy with doing this logic in the services.
+         * The more I expand this the more I think I should have used a library for this .___.
          * 
          */
         GetEmployeeDto MapEmployeeToDto(Employee employee);
+        Employee MapDtoToEmployee(GetEmployeeDto employee);
         GetDependentDto MapDependentToDto(Dependent dependent);
+        Dependent MapDtoToDependent(GetDependentDto dependentDto);
+        GetEmployeePaycheckDto MapEmployeePaychecksToDto(Employee employee, List<Paycheck> paychecks);
+        PaycheckDto MapPaycheckToDto(Paycheck paycheck);
+        PaycheckDeductionDto MapDeductionToDto(Deduction deduction);
     }
 }
--- PaylocityBenefitsCalculator/Api/Services/Mapping/MapperService.cs
+++ PaylocityBenefitsCalculator/Api/Services/Mapping/MapperService.cs
@@ -1,5 +1,6 @@
 ﻿using Api.Dtos.Dependent;
 using Api.Dtos.Employee;
+using Api.Dtos.Paycheck;
 using Api.Models;
 
 namespace Api.Services.Mapping
@@ -18,6 +19,19 @@ namespace Api.Services.Mapping
             };
         }
 
+        public Employee MapDtoToEmployee(GetEmployeeDto employeeDto)
+        {
+            return new Employee
+            {
+                Id = employeeDto.Id,
+                FirstName = employeeDto.FirstName,
+                LastName = employeeDto.LastName,
+                Salary = employeeDto.Salary,
+                DateOfBirth = employeeDto.DateOfBirth,
+                Dependents = employeeDto.Dependents.Select(MapDtoToDependent).ToList()
+            };
+        }
+
         public GetDependentDto MapDependentToDto(Dependent dependent)
         {
             return new GetDependentDto
@@ -27,6 +41,50 @@ namespace Api.Services.Mapping
                 LastName = dependent.LastName,
                 DateOfBirth = dependent.DateOfBirth,
                 Relationship = dependent.Relationship,
+            };
+        }
+
+        public Dependent MapDtoToDependent(GetDependentDto dependentDto)
+        {
+            return new Dependent
+            {
+                Id = dependentDto.Id,
+                FirstName = dependentDto.FirstName,
+                LastName = dependentDto.LastName,
+                DateOfBirth = dependentDto.DateOfBirth,
+                Relationship = dependentDto.Relationship,
+            };
+        }
+
+        public GetEmployeePaycheckDto MapEmployeePaychecksToDto(Employee employee, List<Paycheck> paychecks)
+        {
+            return new GetEmployeePaycheckDto
+            {
+                EmployeeId = employee.Id,
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                GrossSalary = employee.Salary,
+                TakeHomePay = paychecks.Sum(p => p.TakeHomePay),
+                YearlyPaychecks = paychecks.Select(MapPaycheckToDto).ToList()
+            };
+        }
+
+        public PaycheckDto MapPaycheckToDto(Paycheck paycheck)
+        {
+            return new PaycheckDto
+            {
+                GrossSalary = paycheck.GrossSalary,
+                PaycheckDeductions = paycheck.Deductions.Select(MapDeductionToDto).ToList(),
+                TakeHomePay = paycheck.TakeHomePay
+            };
+        }
+
+        public PaycheckDeductionDto MapDeductionToDto(Deduction deduction)
+        {
+            return new PaycheckDeductionDto
+            {
+                Description = deduction.Description,
+                Amount = deduction.Amount
             };
         }
     }
