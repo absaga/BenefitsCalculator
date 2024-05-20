43d23405dedec8afa3386d34a9c72e17c3569a3d
Abdiel Sanchez-Gaud <gaud.abdiel@gmail.com>
2024-05-19 22:45:03 -0500

    Create service that will handle salary deductions

---
+++ PaylocityBenefitsCalculator/Api/Services/SalaryDeductionCalculatorService.cs
@@ -0,0 +1,32 @@
+﻿using Api.Models;
+using Api.Rules.SalaryDeduction;
+
+namespace Api.Services
+{
+    /*
+     * This service will handle obtaining all deductions for the employee on a yearly basis
+     * by running all the rules and calculating the appropriate deductions.
+     *
+     */
+    public class SalaryDeductionCalculatorService : ISalaryDeductionCalculatorService
+    {
+        private readonly List<ISalaryDeductionRule> _salaryDeductionRules;
+        public SalaryDeductionCalculatorService(List<ISalaryDeductionRule> salaryDeductionRules)
+        {
+            _salaryDeductionRules = salaryDeductionRules;
+        }
+        public List<Deduction> CalculateYearlyDeduction(Employee employee)
+        {
+            var result = new List<Deduction>();
+
+            foreach (var rule in _salaryDeductionRules)
+            {
+                if (rule.AppliesToEmployee(employee))
+                {
+                    result.Add(rule.CalculateYearlyDeduction(employee));
+                }
+            }
+            return result;
+        }
+    }
+}
