539984fa6f5b2a1d708ed7e1f2f221eb7b6beb22
Abdiel Sanchez-Gaud <gaud.abdiel@gmail.com>
2024-05-19 22:33:10 -0500

    Create interface to be used with Salary Deduction Rules

---
+++ PaylocityBenefitsCalculator/Api/Rules/SalaryDeduction/ISalaryDeductionRule.cs
@@ -0,0 +1,17 @@
+﻿using Api.Models;
+
+namespace Api.Rules.SalaryDeduction
+{
+
+    /*
+     * I have some mixed feelings about this one, but I'm thinking of creating this interface to
+     * define the shape of the salary deduction rules. I'm thinking by creating each rule here and registering them
+     * separately, I can make them  more modular and easier to test.
+     * 
+     */
+    public interface ISalaryDeductionRule
+    {
+        bool AppliesToEmployee(Employee employee);
+        Deduction CalculateYearlyDeduction(Employee employee);
+    }
+}
